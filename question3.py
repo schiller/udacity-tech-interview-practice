0201725b8fbc574cccc5edcb8667ce17d9d886c3
Luiz Gustavo Schiller <schillerbr@gmail.com>
2017-05-12 18:40:25 -0300

    Solved question 3

--- question3.py
+++ question3.py
@@ -12,4 +12,50 @@
 # The function definition should be question3(G)
 
 def question3(G):
-  return {}
+  if G is None:
+    return None
+
+  MSP = {}
+  edges = []
+
+  for v1 in G:
+    for (v2, e) in G[v1]:
+      edges.append((v1, v2, e))
+  edges.sort(key = lambda tup: tup[2])
+
+  for (v1, v2, e) in edges:
+    if v1 not in MSP or v2 not in MSP:
+      if v1 in MSP:
+        MSP[v1].append((v2, e))
+      else:
+        MSP[v1] = [(v2, e)]
+      if v2 in MSP:
+        MSP[v2].append((v1, e))
+      else:
+        MSP[v2] = [(v1, e)]
+
+  if len(edges) == 0:
+    MSP = G
+    
+  return MSP
+
+
+print question3(None)
+# None
+
+print question3({})
+# {}
+
+print question3({'A': []})
+# {'A': []}
+
+G = {'A': [('B', 1), ('C', 5), ('D', 4)],
+     'B': [('A', 1), ('C', 2), ('D', 6)],
+     'C': [('A', 5), ('B', 2), ('D', 3)],
+     'D': [('A', 4), ('B', 6), ('C', 3)]}
+
+print question3(G)
+# {'A': [('B', 1)],
+#  'B': [('A', 1), ('C', 2)],
+#  'C': [('B', 2), ('D', 3)],
+#  'D': [('C', 3)]}
